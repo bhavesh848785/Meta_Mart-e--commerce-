5e466ee6cab24f273ee88159c7353db39bb15430
bhavesh848785 <bhaveshvaghela4545@gmail.com>
2024-05-27 18:36:20 +0530

    Initial commit of MetaMart with fronted

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navigation from "./customer/components/Navigation/Navigation";
+import HomePage from "./customer/pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <div>home</div>
+      <div>
+        <HomePage />
+      </div>
     </div>
   );
 }
---
+++ fronted/src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import Button from "@mui/material/Button";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const HomeSectionCarosel = ({ data }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
+  };
+
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+  const slideNext = () => setActiveIndex(activeIndex + 1);
+
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+  const items = data
+    .slice(0, 10)
+    .map((item) => <HomeSectionCard product={item} />);
+
+  // const items = data // Use data directly, no need for slicing
+  //   .map((item) => <HomeSectionCard key={item.imageUrl} product={item} />);
+
+  return (
+    <div className="border">
+      <div className="relative p-5">
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          responsive={responsive}
+          disableDotsControls
+          onSlideChange={syncActiveIndex}
+          activeIndex={activeIndex}
+        />
+
+        {activeIndex !== items.length - 5 && (
+          <Button
+            variant="contained"
+            className="z-50 bg-white"
+            onClick={slideNext}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+        {activeIndex !== 0 && (
+          <Button
+            onClick={slidePrev}
+            variant="contained"
+            className="z-50 bg-white"
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarosel;
